b68e5525627e71af7fbf1058217492ae52179f62
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-26 19:49:48 +0530

    Add files via upload

---
+++ null-coalescing operator/Null_Coalescing_Operator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Nullable_Types_1
+{
+    class Null_Coalescing_Operator
+    {
+        static void Main(string[] args)
+        {
+            int AvailableTickets;
+            int? TicketsOnSale = null;
+
+            AvailableTickets = TicketsOnSale ?? 0;              // with Null coalescing operator ??
+
+
+            //if (TicketsOnSale == null)              //without using Null coalescing operator
+            //{
+            //    AvailableTickets = 0;
+            //}
+            //else
+            //{
+            //    AvailableTickets = (int)TicketsOnSale;
+            //}
+            Console.WriteLine("Available Tickets = {0}", AvailableTickets);
+        }
+    }
+}
